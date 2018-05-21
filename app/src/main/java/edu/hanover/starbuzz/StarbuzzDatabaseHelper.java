99c4ff7e3dd9335c691d496b959b64e7202f2b86
Lucas Miller <millerlu19@hanover.edu>
2018-05-21 11:12:37 -0400

    finished creating new table and added insertDrink function

--- app/src/main/java/edu/hanover/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/edu/hanover/starbuzz/StarbuzzDatabaseHelper.java
@@ -1,5 +1,6 @@
 package edu.hanover.starbuzz;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -15,13 +16,25 @@ class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE DRINK ("
-                + "_id INTEGER PRIMARY KEY AUTOINCREMEMENT, "
+        db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "NAME TEXT, "
                 + "DESCRIPTION TEXT, "
                 + "IMAGE_RESOURCE_ID INTEGER);");
+        insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+        insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
+                R.drawable.cappuccino);
+        insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+
+    private static void insertDrink(SQLiteDatabase db, String name,
+                                    String description, int resourceId) {
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME", name);
+        drinkValues.put("DESCRIPTION", description);
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
+        db.insert("DRINK", null, drinkValues);
+    }
 }
